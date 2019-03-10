d2a46bff37e5ed54110caad7a3d56c4e0b316886
Daniel Cespedes Zorob <daniel.cespedes.zorob@gmail.com>
2019-03-10 16:43:11 -0600

    Commit until pie chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,37 +1,32 @@
 function buildMetadata(sample) {
-
-  filteredData.forEach((UFOReport) => {
-    var row = tbody.append("tr");
-    Object.entries(UFOReport).forEach(([key, value]) => {
-      var cell = tbody.append("td");
-      cell.text(value);
-    });
-  });
-
-  // @TODO: Complete the following function that builds the metadata panel
-
+  // Complete the following function that builds the metadata panel
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-    var metadataPanel = d3.select("#sample-metadata");
-
-    // Use `.html("") to clear any existing metadata
-    metadataPanel.html("");
+    var Panel = d3.select("#sample-metadata");
     
-
+    // Use `.html("") to clear any existing metadata
+    Panel.html("");
+    
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    url_Metadata = "/metadata/" + sample;    
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-    var url = "/metadata/" + sample
+    var Panel = d3.select("#sample-metadata");
+    // Use `.html("") to clear any existing metadata
+    Panel.html("");
 
-    d3.json(url).then(console.log(data));
-    d3.json("/metadata/975").then(console.log(data));
+    // url_Metadata = "/metadata/975";
 
-var data = d3.json("/metadata/975"); 
-console.log(data);
-
+    d3.json(url_Metadata).then((sample_Metadata) => {
+      Object.entries(sample_Metadata).forEach(([key, value]) => {       
+        //  Lets create a row per content in the sample_Metadata
+        var row = Panel.append("h6");       
+        //  Lets create the content that goes on the row (With non-breaking spaces)
+        var content = key+':'+'\xa0'+value;       
+        row.text(content);
+      });
+    });
 
 }
 
@@ -42,6 +37,7 @@ function buildCharts(sample) {
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
+    pie_plot = d3.json()
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
 }
@@ -64,6 +60,27 @@ function init() {
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
+
+  var data = [{
+    values: [19, 26, 55, 88],
+    labels: ["Spotify", "Soundcloud", "Pandora", "Itunes"],
+    type: "pie"
+  }];
+
+  var layout = {
+    height: 600,
+    width: 800
+  };
+
+  Plotly.plot("pie", data, layout);
+
+
+
+
+
+
+
+
 }
 
 function optionChanged(newSample) {
